e9d6f02febb09d6b25af2d3cedd2c21afc8a7e82
md-mridul-ali <mridulaiub17@gmail.com>
2025-11-16 20:53:18 +0600

    fully responsive for all devices

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
 const navLinks = document.querySelectorAll('header nav a');
 const logoLink = document.querySelector('.logo');
 const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
 
 const activePage = () => {
     const header = document.querySelector('header');
@@ -22,6 +29,9 @@ const activePage = () => {
     sections.forEach(section => {
         section.classList.remove('active');
     });
+
+     menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
 navLinks.forEach((link, idx) => {
